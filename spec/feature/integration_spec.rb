b20e532506a01d350e8d53e2b1e70eacf21d579b
“kirk-jgraham3” <kirk.jgraham3@tamu.edu>
2024-01-29 22:56:27 -0600

    All 8 tests created and passing

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'Creating a book', type: :feature do
     visit new_book_path
   end
 
-  it 'creates a book with valid title' do
+  it 'valid title' do
     fill_in 'book[title]', with: 'harry potter'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
 
-  it 'creates a book with valid author' do
+  it 'valid author' do
     fill_in 'book[title]', with: 'harry potter'
     fill_in 'book[author]', with: 'J.K. Rowling'
     click_on 'Create Book'
@@ -21,7 +21,7 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('J.K. Rowling')
   end
 
-  it 'creates a book with valid price' do
+  it 'valid price' do
     fill_in 'book[title]', with: 'harry potter'
     fill_in 'book[price]', with: 19.99
     click_on 'Create Book'
@@ -29,7 +29,7 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('19.99')
   end
 
-  it 'creates a book with valid published date' do
+  it 'valid published date' do
     fill_in 'book[title]', with: 'harry potter'
     select '1997', from: 'book_published_date_1i'
     select 'June', from: 'book_published_date_2i'
